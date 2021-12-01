105ab2d32274703b24fff8ffa687ec425c3e3669
“Enter <anirudhanand984@gmail.com>
2021-12-01 18:14:23 +0530

    [Anirudh] add : Mobile Number Validated

--- RegexUserRegistration/Program.cs
+++ RegexUserRegistration/Program.cs
@@ -11,7 +11,7 @@ namespace RegexUserRegistration
             while (flag)
             {
                 Console.WriteLine("Welcome to the User Registration validation program");
-                Console.WriteLine("Enter the validation to be executed : \n 1. First Name \n 2. LastName\n 3.Email\n 4.Exit");
+                Console.WriteLine("Enter the validation to be executed : \n 1. First Name \n 2. LastName\n 3.Email\n 4.Mobile Number\n 5.Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -25,6 +25,9 @@ namespace RegexUserRegistration
                         Console.WriteLine(validation.ValidateEmail("abc.xyz@bl.co.in"));
                         break;
                     case 4:
+                        Console.WriteLine(validation.ValidateMobileNumber("91 7905142214"));
+                        break;
+                    case 5:
                         flag = false;
                         break;
                 }
