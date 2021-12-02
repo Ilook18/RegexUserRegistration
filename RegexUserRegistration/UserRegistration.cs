9487a00bf5bc78792a418c33928755914439d3b6
Ilook18 <78365360+Ilook18@users.noreply.github.com>
2021-12-02 23:29:54 +0530

    [Anirudh] add : Password Rule 1

--- RegexUserRegistration/UserRegistration.cs
+++ RegexUserRegistration/UserRegistration.cs
@@ -11,7 +11,7 @@ namespace RegexUserRegistration
         public static string REGEX_LAST_NAME = "[A-Z]{1}[a-z]{2,}$";
         public static string REGEX_EMAIL = "^[a-z]{3,}[_+-.]{0,1}[a-z]{3,}[@][a-z]{2,}[.][a-z]{2}[.][a-z]{2}$";
         public static string REGEX_MOBILE = "^[1,9]{2} [6-9]{1}[0-9]{9}$";
-         public static string REGEX_PASSWORD_1 = "^(?=.{8,})";
+        public static string REGEX_PASSWORD_1 = "^(?=.{8,})"; 
         public bool ValidateFirstName(string firstName)
         {
             return Regex.IsMatch(firstName, REGEX_FIRST_NAME);
