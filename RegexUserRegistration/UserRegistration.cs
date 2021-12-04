30821045a39cc7ce2fced80ca41dc145b6371864
Ilook18 <78365360+Ilook18@users.noreply.github.com>
2021-12-04 07:44:40 +0530

    [Anirudh] add : Password Rule 1

--- RegexUserRegistration/UserRegistration.cs
+++ RegexUserRegistration/UserRegistration.cs
@@ -9,7 +9,7 @@ namespace RegexUserRegistration
     {
         public static string REGEX_FIRST_NAME = "^[A-Z]{1}[a-z]{2,}$";
         public static string REGEX_LAST_NAME = "[A-Z]{1}[a-z]{2,}$";
-        public static string REGEX_EMAIL = "^[a-z]{3,}[_+-.]{0,1}[a-z]{3,}[@][a-z]{2,}[.][a-z]{2}[.][a-z]{2}$";
+        public static string REGEX_EMAIL = "^[a-z]{3,}[.][a-z]{3,}[@][a-z]{2,}[.][a-z]{2}[.][a-z]{2}$";
         public static string REGEX_MOBILE = "^[1,9]{2} [6-9]{1}[0-9]{9}$";
         public static string REGEX_PASSWORD_1 = "^(?=.{8,})"; 
         public bool ValidateFirstName(string firstName)
